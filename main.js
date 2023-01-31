afc7c82c9e16cef0dde3da4f8a7151edc0cc8acf
titas.partikas <titas.partikas@devbridge.com>
2023-01-31 21:43:16 +0200

    edit

--- main.js
+++ main.js
@@ -2,13 +2,20 @@ import { createGroceryItem } from './utils/createGroceryItem.js';
 import { getDataFromLocalStorage } from './utils/storageManagement/getDataFromLocalStorage.js';
 import { setupLocalStorage } from './utils/storageManagement/setupLocalStorage.js';
 import { localStorageKeys } from './constants/localStorageKeys.js';
+import { setDataToLocalStorage } from './utils/storageManagement/setDataToLocalStorage.js';
 
 setupLocalStorage();
 
 const addGroceryBtn = document.querySelector('#add');
 const groceryInput = document.querySelector('#grocery-input');
 addGroceryBtn.addEventListener('click', () => {
-    createGroceryItem(groceryInput.value);
+    const grocery = {
+        id: Math.random(),
+        name: groceryInput.value,
+    };
+    createGroceryItem(grocery);
+    const groceries = getDataFromLocalStorage(localStorageKeys.groceries);
+    setDataToLocalStorage(localStorageKeys.groceries, [...groceries, grocery])
 });
 
 const groceriesFromLocalStorage = getDataFromLocalStorage(localStorageKeys.groceries);
--- utils/createGroceryItem.js
+++ utils/createGroceryItem.js
@@ -1,6 +1,9 @@
+import { localStorageKeys } from "../constants/localStorageKeys.js";
+import { getDataFromLocalStorage } from "./storageManagement/getDataFromLocalStorage.js";
+
 const groceriesList = document.querySelector('.groceries');
 
-export const createGroceryItem = (name) => {
+export const createGroceryItem = (grocery) => {
     const itemContainer = document.createElement('div');
     const nameParagraph = document.createElement('p');
     const actionsContainer = document.createElement('div');
@@ -10,13 +13,25 @@ export const createGroceryItem = (name) => {
     const deleteIcon = document.createElement('span');
 
     itemContainer.classList.add('grocery');
-    nameParagraph.textContent = name;
+    nameParagraph.textContent = grocery.name;
     editBtn.classList.add('edit');
     editIcon.classList.add('material-symbols-outlined');
     editIcon.textContent = 'edit';
     deleteBtn.classList.add('remove');
     deleteIcon.classList.add('material-symbols-outlined');
     deleteIcon.textContent = 'delete';
+
+    editBtn.addEventListener('click', () => {
+        const groceries = getDataFromLocalStorage(localStorageKeys.groceries);
+        const editableGroceryIndex = groceries.findIndex((groceryFromLocalStorage) => {
+            if (groceryFromLocalStorage.id === grocery.id) {
+                return true;
+            }
+        });
+    })
+    deleteBtn.addEventListener('click', () => {
+        console.log(grocery);
+    })
 
     editBtn.append(editIcon);
     deleteBtn.append(deleteIcon);
